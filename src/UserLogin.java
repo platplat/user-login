4bacd4a2124cf91a373d2c86062dc9fe48ed0c03
AB <22397218+platplat@users.noreply.github.com>
2026-09-11 18:52:40 +0100

    feat: add password hashing method
    Added method for hashing passwords before storing (SHA-256)

--- src/UserLogin.java
+++ src/UserLogin.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
 import java.util.Map;
 import java.util.HashMap;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.nio.charset.StandardCharsets;
 
 public class UserLogin {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
         Scanner sc = new Scanner(System.in);
 
         // Get user input
@@ -23,17 +26,41 @@ public class UserLogin {
         sc.close();
     }
 
-    public static boolean isValidCredentials(String username, String password) {
+
+    public static boolean isValidCredentials(String username, String password) throws NoSuchAlgorithmException {
         // Starting with hardcoded user details
         Map<String,String> users = new HashMap<>();
-        users.put("user1","password123");
-        users.put("user2", "password321");
+        users.put("user1",getHash("password123"));
+        users.put("user2", getHash("password321"));
 
         // Check if username and password are valid credentials
-        if (users.containsKey(username) && password.equals(users.get(username))) {
+        if (users.containsKey(username) && users.get(username).equals(getHash(password))) {
             return true;
         } else {
             return false;
         }
+    }
+
+
+    public static String getHash(String password) throws NoSuchAlgorithmException {
+        // Get SHA-256 hashing tool
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        
+        // Convert password to byte array
+        byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
+
+        // Hash password bytes
+        byte[] passwordHash = md.digest(passwordBytes);
+
+        // Create StringBuilder object to store output String
+        StringBuilder hexString = new StringBuilder();
+
+        // Iterate through the bytes of the hashed password, conver to hexadecimal and append to string
+        for (byte b: passwordHash) {
+            hexString.append(String.format("%02x", b));
+        }
+
+        // Convert StringBuilder object to a String and return
+        return hexString.toString();
     }
 }
