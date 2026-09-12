d9de6c93c34e82e9603d048bc9ab7763d9f4d527
AB <22397218+platplat@users.noreply.github.com>
2026-09-12 15:14:04 +0100

    feat: add user registration option
    Updated user interface to allow user to choose between login, register, and exit. Seperated login and register interfaces to their own static methods.

--- src/UserLogin.java
+++ src/UserLogin.java
@@ -10,6 +10,30 @@ public class UserLogin {
         UserManagement userManagement = new UserManagement(USERS_FILE);
 
         while (true) {
+            System.out.println("Do you want to:");
+            System.out.println("[1] Login (existing users)");
+            System.out.println("[2] Register (new users)");
+            System.out.println("[3] Exit");
+            String userChoice = sc.nextLine().strip().toLowerCase();
+
+            if (userChoice.equals("1") || userChoice.equals("login")) {
+                startLogin(sc, userManagement);
+                break;
+            } else if (userChoice.equals("2") || userChoice.equals("register")) {
+                startRegistration(sc, userManagement);
+            } else if (userChoice.equals("3") || userChoice.equals("exit")) {
+                break;
+            } else {
+                System.out.println("Invalid option. Please try again.");
+            }
+        }
+
+        // Close scanner
+        sc.close();
+    }
+
+    public static void startLogin(Scanner sc, UserManagement userManagement) {
+        while (true) {
             // Get user input
             System.out.print("Enter username: ");
             String usernameInput = sc.nextLine();
@@ -24,8 +48,33 @@ public class UserLogin {
                 System.out.println("Incorrect username/password. Please try again.");
             }
         }
+    }
 
-        // Close scanner
-        sc.close();
+    public static void startRegistration(Scanner sc, UserManagement userManagement) {
+        while (true) {
+            // Get user input
+            System.out.print("Enter username: ");
+            String usernameInput = sc.nextLine();
+            System.out.print("Enter password: ");
+            String passwordInput = sc.nextLine();
+            System.out.print("Confirm password: ");
+            String passwordConfirm = sc.nextLine();
+
+            while ( !(passwordInput.equals(passwordConfirm)) ) {
+                System.out.println("Passwords do not match. Please try again.");
+                System.out.print("Enter password: ");
+                passwordInput = sc.nextLine();
+                System.out.print("Confirm password: ");
+                passwordConfirm = sc.nextLine();
+            }
+
+            // Register user
+            if (userManagement.addUser("src/users.json", usernameInput, passwordInput)) {
+                System.out.println("User registration successful.");
+                break;
+            } else {
+                System.out.println("Username already exists. Please try again.");
+            }
+        }
     }
 }
